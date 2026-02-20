d33f386b9b03f8d22d78875d10bd06757f354220
Matei Costinescu <mcostinescu213@gmail.com>
2026-02-20 15:05:26 +0000

    Add initial project files and configurations for Emergency Dispatch System

---
+++ src/main/java/Incident.java
@@ -0,0 +1,49 @@
+/**
+ * This class holds data about incidents in emergency calls
+ * @author Matei Costinescu
+ * @version 1.0
+ */
+
+public class Incident{
+    private String type;
+    private String district;
+    private int priority;
+
+    /**
+     * Constructor for the Incident class
+     * @param type
+     * @param district
+     * @param priority
+     */
+    public Incident(String type, String district, int priority){
+        this.type = type;
+        this.district = district;
+        this.priority = priority;
+    }
+
+    // Getters for the Incident class
+
+    /**
+     * Gets the type of the incident
+     * @return type of the incident
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Gets the district of the incident
+     * @return district of the incident
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * Gets the priority of the incident
+     * @return priority of the incident
+     */
+    public int getPriority() {
+        return priority;
+    }
+}
