e5eb2d45ec5bc4a4fb0aef5c59764f39364f2897
Matei Costinescu <mcostinescu213@gmail.com>
2026-02-21 17:05:52 +0000

    Add generic SystemLog class to manage log entries and provide logging functionality

--- src/main/java/SystemLog.java
+++ src/main/java/SystemLog.java
@@ -1,5 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class SystemLog {
+/**
+ * This class is responsible for logging all incidents and dispatches in the system.
+ * It maintains a list of log entries and provides methods to add new entries and retrieve the log history.
+ *
+ * @param <T> The type of log entries to be stored in the system log.
+ * @author Matei Costinescu
+ * @version 1.0
+ */
+public class SystemLog<T> {
+    private List<T> logItems;
+
+    /**
+     * Constructor for the SystemLog class. Initializes the logItems list.
+     */
+    public SystemLog() {
+        this.logItems = new ArrayList<>();
+    }
+
+    /**
+     * Adds a new entry to the system log.
+     *
+     * @param item The item to be added to the log.
+     */
+    public void add(T item) {
+        logItems.add(item);
+    }
 }
